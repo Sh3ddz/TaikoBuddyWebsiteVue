7f591854db86819234c98eeaa70c9ba68bcf35bc
Sh3ddz <Sh3ddz@gmail.com>
2024-03-11 22:05:41 -0400

    Re-implemented other components
    -Button
    -Accordion
    -Calendar
    -DropDownMenu
    also, added light / dark mode switch

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,68 @@
 <script setup lang="ts">
+
 import HelloWorld from './components/HelloWorld.vue'
+
+import { useColorMode } from '@vueuse/core'
+import { Icon } from '@iconify/vue'
+
 import { Button } from '@/components/ui/button'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+import { Calendar } from '@/components/ui/calendar'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { ref } from 'vue'
+
+const count = ref(0)
+const defaultValue = 'item-1'
+const accordionItems = [
+  { value: 'item-1', title: 'Is it accessible?', content: 'Yes. It adheres to the WAI-ARIA design pattern.' },
+  { value: 'item-2', title: 'Is it unstyled?', content: 'Yes. It\'s unstyled by default, giving you freedom over the look and feel.' },
+  { value: 'item-3', title: 'Can it be animated?', content: 'Yes! You can use the transition prop to configure the animation.' },
+]
+const date = ref(new Date())
+const mode = useColorMode()
 </script>
 
 <template>
+  
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline">
+        <Icon icon="radix-icons:moon" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Icon icon="radix-icons:sun" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <span class="sr-only">Toggle theme</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="mode = 'light'">
+        Light
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'dark'">
+        Dark
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'auto'">
+        System
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+
+  <Button @click="count++">Click me! {{ count }}</Button>
+  <Button variant="destructive">Click me</Button>
+  <Button variant="outline">Click me</Button>
+  <Button variant="secondary">Click me</Button>
+  <Button variant="ghost">Click me</Button>
+
+  <Accordion type="single" class="w-full" collapsible :default-value="defaultValue">
+    <AccordionItem v-for="item in accordionItems" :key="item.value" :value="item.value">
+      <AccordionTrigger>{{ item.title }}</AccordionTrigger>
+      <AccordionContent>
+        {{ item.content }}
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+
+  <Calendar v-model="date" class="rounded-md border" />
+  <Button>{{ date }}</Button>
+
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -12,7 +71,7 @@ import { Button } from '@/components/ui/button'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <Button>Click me</Button>
+
   <HelloWorld msg="Vite + Vue" />
 </template>
 
