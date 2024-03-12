26aac08c77ade10445b3d7dffe0de39e8e62f167
Sh3ddz <Sh3ddz@gmail.com>
2024-03-11 21:42:41 -0400

    Re-did project initialization
    To change the file structure, the project was re-initialized

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import path from "path"
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import tailwind from "tailwindcss"
+import autoprefixer from "autoprefixer"
 
-// Get the directory path of the current file
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': `${__dirname}/src` // Use the __dirname variable to resolve the path
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
